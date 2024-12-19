43b2b4ccb45f03ea3e51b50307b9b594e58f8f39
EstevaoFernande744 <estevaofernandes744@gmail.com>
2024-12-19 11:49:12 -0300

    try dev with java

---
+++ demo/src/main/java/com/example/Main.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Main {
+   public Main() {
+   }
+
+   
+
+   public static void main(String[] var0) {
+      System.out.println("Hello world!");
+      
+   }
+}
