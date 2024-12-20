935f91b35a4356bc79eddeb27bc11acfcfbc8302
EstevaoFernande744 <estevaofernandes744@gmail.com>
2024-12-20 15:19:12 -0300

    ExR009

---
+++ src/main/java/ExResolvidos/ExR009.java
@@ -0,0 +1,23 @@
+package ExResolvidos;
+
+import java.util.Scanner;
+
+public class ExR009 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um número!");
+
+        int numero = sc.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.printf("O número %d é Par!", numero);
+        }
+        else{
+            System.out.printf("O número %d é Ímpar!", numero);
+        }
+
+        sc.close();
+    }
+}
