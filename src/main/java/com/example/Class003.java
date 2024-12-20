ca783985af4ddef27cf8bdc188848668d837bc17
EstevaoFernande744 <estevaofernandes744@gmail.com>
2024-12-20 08:40:27 -0300

    ex002 class003

---
+++ src/main/java/com/example/Class003.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Class003 {
+    public static void main(String[] args) {
+
+        int x;
+        double y;
+
+        x = 5;
+        y = 2 * x;
+
+        System.out.printf("Os valores de x e y são %d e %.2f", x, y);
+
+    }
+}
---
+++ src/main/java/com/example/Ex002.java
@@ -0,0 +1,27 @@
+package com.example;
+
+public class Ex002 {
+
+    public static void main(String[] args) {
+
+        double b, B, h, area;
+
+        b = 6.0;
+        B = 8.0;
+        h = 5.0;
+
+        area = (b + B)/ 2.0 * h;
+
+        System.out.printf("Essa é a area do trapezio %.2f\n", area);
+
+        int c, d;
+        double resultado;
+
+        c = 5;
+        d = 2;
+
+        resultado = (double) c / d;
+
+        System.out.printf("A divisão de %d e %d é %.2f",  c, d, resultado);
+    }
+}
