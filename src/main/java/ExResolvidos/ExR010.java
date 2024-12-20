12ddeb55f821b1b237de68ded22dd4be74702a43
EstevaoFernande744 <estevaofernandes744@gmail.com>
2024-12-20 15:55:21 -0300

    ExR011

---
+++ src/main/java/ExResolvidos/ExR010.java
@@ -0,0 +1,26 @@
+package ExResolvidos;
+
+import java.util.Scanner;
+
+public class ExR010 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Digite dois números:");
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        if(A % B == 0 || B % A == 0){
+            System.out.println("São multiplos!");
+        }
+        else {
+            System.out.println("Não são multiplos!");
+        }
+
+        sc.close();
+    }
+
+}
---
+++ src/main/java/ExResolvidos/ExR011.java
@@ -0,0 +1,28 @@
+package ExResolvidos;
+
+import java.util.Scanner;
+
+public class ExR011 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Digite os valores de inicio e fim do periodo de gameplay:");
+
+        Scanner sc = new Scanner(System.in);
+
+        int horarioInicial = sc.nextInt();
+        int horarioFinal = sc.nextInt();
+        int tempoDeJogo;
+
+        if (horarioFinal > horarioInicial){
+            tempoDeJogo = horarioFinal - horarioInicial;
+        }
+        else {
+            tempoDeJogo = 24 - horarioInicial + horarioFinal;
+        }
+
+        System.out.printf("O jogo durou %d hora(s)!", tempoDeJogo);
+
+        sc.close();
+    }
+}
