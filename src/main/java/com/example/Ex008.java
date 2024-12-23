8de861ccb3229e626ce341ff95d60a6f99e10df3
EstevaoFernande744 <estevaofernandes744@gmail.com>
2024-12-23 16:43:19 -0300

    Ex008

--- src/main/java/com/example/Ex008.java
+++ src/main/java/com/example/Ex008.java
@@ -9,21 +9,36 @@ public class Ex008 {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        /*
         System.out.print("Digite a temperatura em Celsius:");
         double C = sc.nextDouble();
         double F = 9.0 * C/5.0 + 32.0;
         System.out.printf("O equivalente em Fahrenheit: %.1f\n", F);
         System.out.print("Deseja Repetir (s/n)?");
-        char resp = sc.next().charAt(0);
+        */
 
-        while(resp == 's'){
+
+        /*
+        char resp = 's';
+        while(resp != 'n'){
             System.out.print("Digite a temperatura em Celsius:");
-            C = sc.nextDouble();
-            F = 9.0 * C / 5.0 + 32.0;
+            double C = sc.nextDouble();
+            double F = 9.0 * C / 5.0 + 32.0;
             System.out.printf("O equivalente em Fahrenheit: %.1f\n", F);
             System.out.print("Deseja Repetir (s/n)?");
             resp = sc.next().toLowerCase().charAt(0);
         }
+        */
+
+        char resp;
+        do {
+            System.out.print("Digite a temperatura em Celsius:");
+            double C = sc.nextDouble();
+            double F = 9.0 * C / 5.0 + 32.0;
+            System.out.printf("O equivalente em Fahrenheit: %.1f\n", F);
+            System.out.print("Deseja Repetir (s/n)?");
+            resp = sc.next().toLowerCase().charAt(0);
+        } while(resp != 'n');
 
         sc.close();
     }
