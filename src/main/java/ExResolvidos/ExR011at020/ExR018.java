70c0ec7cf8c47f08b677f495528b5df23e248921
EstevaoFernande744 <estevaofernandes744@gmail.com>
2024-12-23 09:33:26 -0300

    ExR018

---
+++ src/main/java/ExResolvidos/ExR011at020/ExR018.java
@@ -0,0 +1,40 @@
+package ExResolvidos.ExR011at020;
+
+import java.util.Scanner;
+
+public class ExR018 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Digite um valor!");
+
+        Scanner sc = new Scanner (System.in);
+
+        int alcool = 0;
+        int gasolina = 0;
+        int diesel = 0;
+
+        int tipo = sc.nextInt();
+
+        while(tipo != 4){
+            if (tipo == 1){
+                alcool += 1;
+            }
+            else if(tipo == 2){
+                gasolina += 1;
+            }
+            else if(tipo == 3){
+                diesel += 1;
+            }
+
+            tipo = sc.nextInt();
+        }
+
+        System.out.println("MUITO OBRIGADO!");
+        System.out.println("Alcool: " + alcool);
+        System.out.println("Gasolina: " + gasolina);
+        System.out.println("Diesel: " + diesel);
+
+        sc.close();
+    }
+}
