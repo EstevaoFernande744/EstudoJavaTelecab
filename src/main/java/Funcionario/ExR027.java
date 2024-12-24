12839cbb628fb13855a7000345711f7f0c41b595
EstevaoFernande744 <estevaofernandes744@gmail.com>
2024-12-24 17:20:25 -0300

    Funcionario

---
+++ src/main/java/Funcionario/ExR027.java
@@ -0,0 +1,31 @@
+package Funcionario;
+
+import java.util.Scanner;
+
+public class ExR027 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Funcionario func = new Funcionario();
+
+        System.out.print("Nome: ");
+        func.nome = sc.nextLine();
+        System.out.print("Salario Solido: ");
+        func.salarioS = sc.nextDouble();
+        System.out.print("taxa: ");
+        func.taxa = sc.nextDouble();
+
+        System.out.println();
+        System.out.println("Funcionario: " + func);
+        System.out.println();
+        System.out.print("Qual porcentagem para aumentar o salário? ");
+        double porcentagem = sc.nextDouble();
+        func.aumentoSalario(porcentagem);
+
+        System.out.println();
+        System.out.println("Dados atualizados: " + func);
+
+        sc.close();
+    }
+}
---
+++ src/main/java/Funcionario/Funcionario.java
@@ -0,0 +1,19 @@
+package Funcionario;
+
+public class Funcionario {
+
+    public String nome;
+    public double salarioS;
+    public double taxa;
+
+    public double salarioL() {
+        return salarioS - taxa;
+    }
+    public void aumentoSalario(double porcentagem) {
+        salarioS += salarioS * porcentagem / 100.0;
+    }
+    public String toString() {
+        return nome + ", $ " + String.format("%.2f", salarioL());
+    }
+
+}
