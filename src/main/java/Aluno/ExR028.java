9618ff374bff4a322ccf00750b151d4a54490639
EstevaoFernande744 <estevaofernandes744@gmail.com>
2024-12-26 09:25:02 -0300

    Aluno ExR028

---
+++ src/main/java/Aluno/ExR028.java
@@ -0,0 +1,31 @@
+package Aluno;
+
+import java.util.Scanner;
+
+public class ExR028 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite seu nome: ");
+
+        Aluno alu = new Aluno();
+
+        alu.nome = sc.nextLine();
+        System.out.println("Digite suas 3 notas: ");
+        alu.nota1 = sc.nextDouble();
+        alu.nota2 = sc.nextDouble();
+        alu.nota3 = sc.nextDouble();
+
+        if (alu.notaFinal() < 60.0 ){
+            System.out.println("4° prova");
+            System.out.printf(" Precisa de %.2f pontos.\n", alu.pontos());
+        }
+        else {
+            System.out.printf("Aprovado! com %.2f", alu.notaFinal());
+        }
+
+
+
+        sc.close();
+    }
+}
