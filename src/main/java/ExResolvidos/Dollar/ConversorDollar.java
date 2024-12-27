e8b09aab1206f38714468083ba8f5caca85cfa7e
EstevaoFernande744 <estevaofernandes744@gmail.com>
2024-12-27 10:40:02 -0300

    CC on ExR032

--- src/main/java/ExResolvidos/Dollar/ConversorDollar.java
+++ src/main/java/ExResolvidos/Dollar/ConversorDollar.java
@@ -1,10 +1,10 @@
 package ExResolvidos.Dollar;
 
-public class ConversorDollar {
+public class ConversorDollar { // nome da class inteira para ser chamada
 
-        public static double IOF = 0.06;
+        public static double IOF = 0.06; // uma das variaveis utilizadas extatica
 
-        public static double dollarToReal(double valor, double preçoDollar){
-            return valor * preçoDollar * (1.0 + IOF);
+        public static double dollarToReal(double valor, double preçoDollar){ // calculo utilizado, com os dois parametros
+            return valor * preçoDollar * (1.0 + IOF); // sem o nome void, obrigatorio o return
         }
 }
